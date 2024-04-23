9a4866f05c5f37df72d7ae1cfc46e0dfbec39805
Jalinsy <jiaji200324@gmail.com>
2024-04-23 10:24:24 +0900

    test

---
+++ BinaryTest.java
@@ -0,0 +1,17 @@
+public class BinaryTest {
+	public static void main(String[] args) {
+		
+		int n1 = 0b1010;
+		//n1是 二进制
+		int n2 = 1010;
+		//n2是 十进制
+		int n3 = 01010;
+		//n3是 八进制
+		int n4 = 0X10101;
+		//n4是 十六进制
+		System.out.println("n1=" + n1);
+		System.out.println("n2=" + n2);
+		System.out.println("n3=" + n3);
+		System.out.println("n4=" + n4);
+	}
+}
